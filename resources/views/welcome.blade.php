9adced92a3e1e3f9edf3114955d79a1c14d407e3
Dragica Gjosheva Zafirova <zimzelena6130@gmail.com>
2025-08-02 07:41:41 +0200

    xxxx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         <!-- Styles / Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.scss', 'resources/js/app.js'])
     </head>
     <body>
         <div id="app"></div>
